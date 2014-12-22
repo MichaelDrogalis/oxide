042d6cae90adfd4fd9e62a674ade6b6b5dd0aff8
Michael Drogalis <mjd3089@rit.edu>
2014-12-22 14:48:07 -0500

    Grammar.

--- src/clj/oxide/grammar.clj
+++ src/clj/oxide/grammar.clj
@@ -1,3 +1,19 @@
 (ns oxide.grammar
   (:require [instaparse.core :as insta]))
 
+(def oxide-grammar
+  (insta/parser
+   "OxideForm = '(' FullExpr ')'
+    FullExpr = VisualFn Form
+    Form = Whitespace+ '(' (PartialExpr | DataSet) ')'
+    PartialExpr = Function Arg+
+    Arg = Whitespace+ Constant Whitespace* | Form
+    VisualFn = 'histogram' | 'locate-on-map' | 'table' | 'blurb'
+    Function = 'within-location' | 'minimum-popularity' | 'group-by-popularity'
+    DataSet = 'data-set' Whitespace+ String
+    String = '\"' (#'[a-zA-Z]' | Whitespace+)+ '\"'
+    Constant = 'x'
+    Whitespace = #'\\s+'"))
+
+(oxide-grammar "(histogram (group-by-popularity (minimum-popularity (within-location (data-set \"Yelp Businesses\") x))))")
+
