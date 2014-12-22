c48bf1ad055822f682e604b3abe59d6c67adc177
Michael Drogalis <mjd3089@rit.edu>
2014-12-22 15:01:43 -0500

    Manually unroll.

--- src/clj/oxide/grammar.clj
+++ src/clj/oxide/grammar.clj
@@ -3,17 +3,37 @@
 
 (def oxide-grammar
   (insta/parser
-   "OxideForm = '(' FullExpr ')'
+   "OxideForm = <'('> FullExpr <')'>
     FullExpr = VisualFn Form
-    Form = Whitespace+ '(' (PartialExpr | DataSet) ')'
+    Form = <Whitespace+> <'('> (PartialExpr | DataSet) <')'>
     PartialExpr = Function Arg+
-    Arg = Whitespace+ Constant Whitespace* | Form
+    Arg = <Whitespace+> Constant <Whitespace*> | Form
     VisualFn = 'histogram' | 'locate-on-map' | 'table' | 'blurb'
     Function = 'within-location' | 'minimum-popularity' | 'group-by-popularity'
-    DataSet = 'data-set' Whitespace+ String
-    String = '\"' (#'[a-zA-Z]' | Whitespace+)+ '\"'
+    DataSet = 'data-set' <Whitespace+> String
+    String = <'\"'> (#'[a-zA-Z]' | Whitespace+)+ <'\"'>
     Constant = String | #'[0-9]'+
     Whitespace = #'\\s+'"))
 
-(oxide-grammar "(histogram (group-by-popularity (minimum-popularity (within-location (data-set \"Yelp Businesses\") 3))))")
+(def parsed (oxide-grammar "(histogram (group-by-popularity (minimum-popularity (within-location (data-set \"Yelp Businesses\") 3))))"))
+
+(def visual-fn (second (second (second parsed))))
+
+(def tree-1 (nth (second parsed) 2))
+
+(def group-by-popularity (second (second (second tree-1))))
+
+(def tree-2 (second (nth (second tree-1) 2)))
+
+(def min-popularity (second (second (second tree-2))))
+
+(def tree-3 (second (nth (second tree-2) 2)))
+
+(def within-location (second (second (second tree-3))))
+
+(def tree-4 (second (nth (second tree-3) 2)))
+
+(def data-set (rest (nth (second tree-4) 2)))
+
+(clojure.pprint/pprint data-set)
 
